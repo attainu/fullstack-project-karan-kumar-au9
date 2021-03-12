25d82db125eaf743b7d19dc3fbfebd46867e9d15
Vinayak Jaiswal <vinayakjaiswal121@gmail.com>
2021-03-12 08:35:46 +0530

    created schema and new products saved

---
+++ backend/models/product.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter the product name"],
+    trim: true,
+    maxlength: [100, "Product name cannot exceed 100 characters"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter the price"],
+    maxlength: [6, "Product price cannot exceed 6 characters"],
+    default: 0.0,
+  },
+  discription: {
+    type: String,
+    required: [true, "Please enter discription"],
+  },
+  rating: {
+    type: Number,
+    default: 0.0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please enter the product category"],
+    enum: {
+      values: [
+        "Electronic",
+        "Cameras",
+        "Laptop",
+        "Accessories",
+        "HeadPhone",
+        "Books",
+        "Phone",
+        "Clothes/Shoes",
+        "Beauty/Health",
+        "Sports",
+        "Outdoor",
+        "Home",
+      ],
+      message: "Please select the correct category for product",
+    },
+  },
+  seller: {
+    type: String,
+    required: [true, "Please enter product seller"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter product stock"],
+    default: 0,
+  },
+  numOfReviews: {
+    type: String,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
