82fa73e2a0572b0c82ca1028bac0dc829df221f0
karan-kumar-au9 <64788698+karan-kumar-au9@users.noreply.github.com>
2021-03-12 11:09:49 +0530

    Some Changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    mongoose.connect('process.env.DB_LOCAL_URI', {
+    mongoose.connect(process.env.DB_LOCAL_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
     }).then(con => {
-        console.log(`MongoDB Database Connected with HOST: ${con.connection.host}`)
+        console.log(`git MongoDB Database Connected with`)
     })
 }
 
