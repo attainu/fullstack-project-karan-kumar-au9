2400c76cb048b444fb01112e5d91a40aa21ea249
Vinayak Jaiswal <vinayakjaiswal121@gmail.com>
2021-03-16 19:30:38 +0530

    adding seperate error function

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
+const errorMiddleware = require("./middlewares/error");
 
 app.use(express.json());
 
 //Import all routes
-const products = require('./routes/product');
+const products = require("./routes/product");
 
+app.use("/api/v1", products);
 
-app.use('/api/v1', products)
+//Middleware to handle error
+app.use(errorMiddleware);
 
-module.exports = app
+module.exports = app;
