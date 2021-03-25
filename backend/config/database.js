5b0b390281fdbeb5510084f6e69476d7e6bbfb07
Vinayak Jaiswal <vinayakjaiswal121@gmail.com>
2021-03-25 07:58:49 +0530

    frontend redux

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
 
 const connectDatabase = () => {
-  mongoose
-    .connect(process.env.DB_LOCAL_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
+    mongoose.connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }).then(con => {
+        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
     })
-    .then((con) => {
-      console.log(
-        `MongoDB Database Connected with HOST: ${con.connection.host}`
-      );
-    });
-};
+}
 
-module.exports = connectDatabase;
+module.exports = connectDatabase
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,21 +1,24 @@
-//Create and send token and store in cookie
+// Create and send token and save in the cookie.
 const sendToken = (user, statusCode, res) => {
-  //create jwt token
-  const sendToken = user.getJwtToken();
 
-  //options for cookie
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE_TIME * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
+    // Create Jwt token
+    const token = user.getJwtToken();
 
-  res.status(statusCode).cookie("token", token, options).json({
-    success: true,
-    token,
-    user,
-  });
-};
+    // Options for cookie
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
+
+
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        token,
+        user
+    })
+
+}
 
 module.exports = sendToken;
